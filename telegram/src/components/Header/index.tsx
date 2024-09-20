eab19fec1b7d697802b792d922d72aa852393f3c
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-09-21 03:46:38 +0700

    fixes

--- telegram/src/components/Header/index.tsx
+++ telegram/src/components/Header/index.tsx
@@ -1,47 +1,60 @@
 import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, Image, Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
-import { FC } from "react";
+import { initBackButton, useInitData, useLaunchParams } from "@telegram-apps/sdk-react";
+import { FC, useMemo } from "react";
+import { initUtils } from '@telegram-apps/sdk';
+import { getUserAvatarUrl } from "@/app/constants"; // Varsayalım avatar URL fonksiyonu burada tanımlı
 
-export const Header: FC<any> = ({ color, className, ...rest }) => (
-    <>
-        <Navbar className="bg-transparent" isBlurred={false}>
-            <NavbarBrand>
-                <Image width={48} height={48} src="/mag.svg" />
-            </NavbarBrand>
-            <NavbarContent justify="center">
-                <Image width={200} src="/logo.webp" />
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <Dropdown placement="bottom-end">
-                    <DropdownTrigger>
-                        <Avatar
-                            isBordered
-                            as="button"
-                            className="transition-transform"
-                            color="secondary"
-                            name="Jason Hughes"
-                            size="sm"
-                            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                        />
-                    </DropdownTrigger>
-                    <DropdownMenu aria-label="Profile Actions" variant="flat">
-                        <DropdownItem key="profile" className="h-14 gap-2">
-                            <p className="font-semibold">Signed in as</p>
-                            <p className="font-semibold">zoey@example.com</p>
-                        </DropdownItem>
-                        <DropdownItem key="settings">My Settings</DropdownItem>
-                        <DropdownItem key="team_settings">Team Settings</DropdownItem>
-                        <DropdownItem key="analytics">Analytics</DropdownItem>
-                        <DropdownItem key="system">System</DropdownItem>
-                        <DropdownItem key="configurations">Configurations</DropdownItem>
-                        <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-                        <DropdownItem key="logout" color="danger">
-                            Log Out
-                        </DropdownItem>
-                    </DropdownMenu>
-                </Dropdown>
-            </NavbarContent>
+export const Header: FC<any> = ({ color, className, ...rest }) => {
+    const [backButton] = initBackButton();
 
-        </Navbar>
-    </>
+    const lp = useLaunchParams();
+    const initData = useInitData();
+    const utils = initUtils();
 
-)
+    const userRows = useMemo<any | undefined>(() => {
+        return initData && initData.user ? initData.user : undefined;
+    }, [initData]);
+
+    const avatarUrl = getUserAvatarUrl(userRows?.id) 
+
+    return (
+        <>
+            <Navbar className="bg-transparent" isBlurred={false}>
+                <NavbarBrand>
+                    <Image width={48} height={48} src="/mag.svg" />
+                </NavbarBrand>
+                <NavbarContent justify="center">
+                    <Image width={200} src="/logo.webp" />
+                </NavbarContent>
+                <NavbarContent justify="end">
+                    <Dropdown placement="bottom-end">
+                        <DropdownTrigger>
+                            <Avatar
+                                isBordered
+                                as="button"
+                                className="transition-transform"
+                                color="secondary"
+                                name={userRows ? userRows.first_name : "User"}
+                                size="sm"
+                                src={avatarUrl}  // Kullanıcının avatarı
+                            />
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Profile Actions" variant="flat">
+                            <DropdownItem key="profile" className="h-14 gap-2">
+                                <p className="font-semibold">Signed in as</p>
+                                <p className="font-semibold">{userRows ? userRows.username : "zoey@example.com"}</p>
+                            </DropdownItem>
+                        
+                            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+                            <DropdownItem onClick={()=>{
+
+                                }} key="logout" color="danger">
+                                Log Out
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </NavbarContent>
+            </Navbar>
+        </>
+    );
+};
