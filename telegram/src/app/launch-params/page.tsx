9e60be2d0b87cc67d839c631cedd010645a28d84
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-09-21 02:12:09 +0700

    fixes

--- telegram/src/app/launch-params/page.tsx
+++ telegram/src/app/launch-params/page.tsx
@@ -7,12 +7,13 @@ import { DisplayData } from '@/components/DisplayData/DisplayData';
 
 export default function LaunchParamsPage() {
   const lp = useLaunchParams();
+  console.log("ERSAN",lp)
 
   return (
     <List>
       <DisplayData
         rows={[
-          { title: 'tgWebAppPlatform', value: lp.platform },
+          { title: 'tgWebAppPlatform', value : lp.platform },
           { title: 'tgWebAppShowSettings', value: lp.showSettings },
           { title: 'tgWebAppVersion', value: lp.version },
           { title: 'tgWebAppBotInline', value: lp.botInline },
